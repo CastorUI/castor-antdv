8fc0fb5d11ff51010cb64919467a72a0dc62b3cc
ReganHe <heoo547627@126.com>
2024-05-29 15:59:13 +0800

    引入AntDesignVue组件库

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,10 +1,13 @@
-import theme from 'vitepress/dist/client/theme-default/index.js'
-import { AntDesignContainer } from '@vitepress-demo-preview/component'
-import '@vitepress-demo-preview/component/dist/style.css'
+import Antd from 'ant-design-vue';
+import 'ant-design-vue/dist/reset.css';
+import theme from 'vitepress/dist/client/theme-default/index.js';
+import { AntDesignContainer } from '@vitepress-demo-preview/component';
+import '@vitepress-demo-preview/component/dist/style.css';
 
 export default {
   ...theme,
-  enhanceApp({app}) {
-    app.component('demo-preview', AntDesignContainer)
-  }
-}
+  enhanceApp({ app }) {
+    app.use(Antd);
+    app.component('demo-preview', AntDesignContainer);
+  },
+};
