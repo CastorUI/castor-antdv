71df5cde68efd7ff8d0be9b71d515b85eeffb403
ReganHe <heoo547627@126.com>
2024-05-29 17:47:19 +0800

    导入Vue组件&tsconfig配置

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -4,10 +4,13 @@ import theme from 'vitepress/dist/client/theme-default/index.js';
 import { AntDesignContainer } from '@vitepress-demo-preview/component';
 import '@vitepress-demo-preview/component/dist/style.css';
 
+import Castor3UI from './../../../packages/index';
+
 export default {
   ...theme,
   enhanceApp({ app }) {
     app.use(Antd);
+    app.use(Castor3UI);
     app.component('demo-preview', AntDesignContainer);
   },
 };
--- docs/components/basic-component2.md
+++ docs/components/basic-component2.md
@@ -1,1 +1,3 @@
 # basic-component2
+
+<preview path="../demos/button-demo-2.vue" title="基本使用" description="组件引入测试"></preview>
