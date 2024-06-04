1dc12c0bad803619ca1971f18231f3665365e02c
ReganHe <heoo547627@126.com>
2024-06-04 13:41:17 +0800

    新增CaCommonTable组件

---
+++ packages/CaCommonTable/index.ts
@@ -0,0 +1,7 @@
+import CaCommonTable from './src/common-table.vue';
+
+CaCommonTable.install = function (Vue) {
+  Vue.component(CaCommonTable.name, CaCommonTable);
+};
+
+export default CaCommonTable;
