b3e83f615bcb4b422a59e4a63f0e3d071e4626a1
ReganHe <heoo547627@126.com>
2024-06-12 22:22:57 +0800

    Vue3组件库-VitePress-Vue组件引入方式优化

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,24 +1,18 @@
-import Antd from 'ant-design-vue';
-import 'ant-design-vue/dist/reset.css';
-import theme from 'vitepress/dist/client/theme-default/index.js';
-import { AntDesignContainer } from '@vitepress-demo-preview/component';
-import '@vitepress-demo-preview/component/dist/style.css';
-import './custom.css';
+import Antd from 'ant-design-vue'
+import 'ant-design-vue/dist/reset.css'
+import theme from 'vitepress/dist/client/theme-default/index.js'
+import { AntDesignContainer } from '@vitepress-demo-preview/component'
+import '@vitepress-demo-preview/component/dist/style.css'
+import './custom.css'
 
-import CaCommonTable from './../../../packages/CaCommonTable/src/common-table.vue';
-import CaCommonQuery from './../../../packages/CaCommonQuery/src/common-query.vue';
-import CaCommonForm from './../../../packages/CaCommonForm/src/common-form.vue';
-
-// import Castor3UI from './../../../packages/index';
+import Castor3UI from './../../../packages/index'
 
 export default {
   ...theme,
   enhanceApp({ app }) {
-    app.use(Antd);
-    // app.use(Castor3UI);
-    app.component('demo-preview', AntDesignContainer);
-    app.component('ca-common-table',CaCommonTable);
-    app.component('ca-common-query',CaCommonQuery);
-    app.component('ca-common-form',CaCommonForm);
-  },
-};
+    app.use(Antd)
+    console.log('Castor3UI', Castor3UI)
+    app.use(Castor3UI)
+    app.component('demo-preview', AntDesignContainer)
+  }
+}
