ec155494a2375df7c2075a1e0291b107ee5cbca5
ReganHe <heoo547627@126.com>
2025-07-11 00:29:11 +0800

    CastorAntdv-表单组件-新增单图片组件

--- types/castor-antd.d.ts
+++ types/castor-antd.d.ts
@@ -74,6 +74,7 @@ export interface FormField {
   | 'autocomplete'
   | 'switch'
   | 'space'
+  | 'image'
   | 'default'
   label: string
   dataField: string
@@ -114,6 +115,13 @@ export interface FormFieldExtendProps {
   showInFooter?: boolean
   /** 分值标题-副标题 */
   subTitle?: string
+  /** 图片选项 */
+  imageOptions?: {
+    width?: string
+    height?: string
+    radius?: string
+    style?: Object
+  }
 }
 
 /** 通用表单 */
